a747ee9c18a839e4cc26262f972373826cf4bc82
Gibosi George <georgegibosi850@gmail.com>
2026-07-08 04:09:59 +0300

    url embed

--- config/api-config.js
+++ config/api-config.js
@@ -3,6 +3,6 @@
 
 export const AppConfig = {
     // Change this when deploying to production (e.g., 'https://api.yourdomain.com/api')
-    API_BASE_URL: 'http://localhost:8080/api',
+    API_BASE_URL: 'https://mylife-portfolio-backend.onrender.com',
     ENVIRONMENT: 'development'
 };
